1b25e8b0120fba8c1a3867290b4bf9003e4f85a5
GangGuo <1289337505@qq.com>
2017-02-27 17:46:21 +0800

    Initial Commit

---
+++ HYOrderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYOrderDemo
+//
+//  Created by David on 2017/2/27.
+//  Copyright © 2017年 GangZi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYOrderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYOrderDemo
+//
+//  Created by David on 2017/2/27.
+//  Copyright © 2017年 GangZi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
